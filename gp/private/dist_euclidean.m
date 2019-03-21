bde37ea4c0da07322f876114cdcbe17a76e8231b
henrylifei <henrylifeipeng@gmail.com>
2019-03-21 16:26:11 +0800

    test commit

---
+++ gp/private/dist_euclidean.m
@@ -0,0 +1,28 @@
+<<<<<<< HEAD
+function dist = dist_euclidean(metric, x)
+% DIST_EUCLIDEAN   is a mex file that returns the euclidean distance matrix
+% for metric_euclidean.  If we end up here the mex file has not been
+% compiled. In this case the function returns NaN and the distance is
+% evaluated in metric_euclidean.
+
+% Copyright (c) 2008 Jarno Vanhatalo     
+
+% This software is distributed under the GNU General Public
+% License (version 3 or later); please refer to the file
+% License.txt, included with the software, for details.
+
+=======
+function dist = dist_euclidean(metric, x)
+% DIST_EUCLIDEAN   is a mex file that returns the euclidean distance matrix
+% for metric_euclidean.  If we end up here the mex file has not been
+% compiled. In this case the function returns NaN and the distance is
+% evaluated in metric_euclidean.
+
+% Copyright (c) 2008 Jarno Vanhatalo     
+
+% This software is distributed under the GNU General Public
+% License (version 3 or later); please refer to the file
+% License.txt, included with the software, for details.
+
+>>>>>>> 273ea2dafe195a745e1c3c5f725e4e574b6c923b
+dist = NaN;
